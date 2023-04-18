7bdcb6e5513774341c925eac9a40db7530096e10
SoDOOManiac <zodomaniac@gmail.com>
2023-04-18 21:18:44 +0300

    Applied @JNechaevsky 's vsync handling

--- src/doom/m_crispy.c
+++ src/doom/m_crispy.c
@@ -791,11 +791,6 @@ void M_CrispyToggleTranslucency(int choice)
     ChangeSettingEnum(&crispy->translucency, choice, NUM_TRANSLUCENCY);
 }
 
-void M_CrispyReInitGraphicsHook (void)
-{
-    I_ReInitGraphics(REINIT_RENDERER | REINIT_TEXTURES | REINIT_ASPECTRATIO);
-}
-
 void M_CrispyToggleUncapped(int choice)
 {
     int old_crispy_vsync = crispy->vsync;
@@ -815,7 +810,7 @@ void M_CrispyToggleUncapped(int choice)
         crispy->vsync = (crispy->uncapped != 1) && (crispy->uncapped != 2); // if crispy->uncapped = 0, vsync is on in case of force_software_renderer == 0
     }
     if (crispy->vsync != old_crispy_vsync)
-        crispy->post_rendering_hook = M_CrispyReInitGraphicsHook;
+        I_ToggleVsync();
 }
 
 void M_CrispyToggleVsyncHook (void)
